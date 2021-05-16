e2a4a8a572d2b55fe3649bccd3c08c0500cf6d29
ink8bit <ink8bit@users.noreply.github.com>
2021-05-16 19:17:11 +0300

    Add logic for "list" command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use serde::Deserialize;
+use reqwest::header::{ACCEPT, USER_AGENT};
+// use reqwest::Error;
+use serde::{Deserialize, Serialize};
 
+use std::env;
 use std::fs;
 
 mod cli;
@@ -16,11 +19,20 @@ fn main() {
         Err(e) => return eprint!("Config file not found: {}", e),
     };
 
-    dbg!(&config);
-    dbg!(&list);
-    dbg!(&update);
+    let repo = config.repo;
+    let owner = config.owner;
+
+    if list {
+        if let Err(e) = view_labels(&owner, &repo) {
+            panic!("{}", e);
+        }
+    }
+
+    if update {
+        todo!();
+    }
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Label {
     name: String,
     description: String,
@@ -50,4 +62,44 @@ impl Config {
 
         Ok(config)
     }
+}
+
+const AUTH_HEADER: &str = "x-oauth-basic";
+
+fn view_labels(owner: &str, repo: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let token = match get_token() {
+        Ok(v) => v,
+        Err(e) => e.to_string(),
+    };
+
+    let request_url = format!(
+        "https://api.github.com/repos/{owner}/{repo}/labels",
+        owner = owner,
+        repo = repo,
+    );
+
+    let response = reqwest::blocking::Client::new()
+        .get(request_url)
+        .basic_auth(token, Some(AUTH_HEADER))
+        .header(ACCEPT, "application/vnd.github.v3+json")
+        .header(USER_AGENT, "labels")
+        .send()?;
+
+    if response.status().is_success() {
+        let labels: Vec<Label> = response.json()?;
+        let pretty = serde_json::to_string_pretty(&labels)?;
+        println!("{}", pretty);
+    } else {
+        eprintln!("{:?} failure", response);
+    }
+
+    Ok(())
+}
+
+fn get_token<'a>() -> Result<String, &'a str> {
+    let token = env::var("LABELS_TOKEN").unwrap_or_default();
+    if token.is_empty() {
+        return Err("Token not found");
+    }
+    Ok(token)
 }
