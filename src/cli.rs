cfc790ae9be57b1e8570a0e2fb53576da0ca3c71
ink8bit <ink8bit@users.noreply.github.com>
2021-05-29 12:16:44 +0300

    Add subcommands - list and update

--- src/cli.rs
+++ src/cli.rs
@@ -1,23 +1,19 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, ArgMatches};
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, ArgMatches};
 
 pub(crate) fn args() -> ArgMatches {
     App::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
         .author(crate_authors!())
-        .arg(
-            Arg::new("list")
-                .short('l')
-                .long("list")
-                .about("Shows labels in current repository (first 100 items)")
-                .conflicts_with("update"),
+        .subcommand(
+            App::new("list")
+                .version(crate_version!())
+                .about("Prints labels in current repository (first 100 items)"),
         )
-        .arg(
-            Arg::new("update")
-                .short('u')
-                .long("update")
-                .about("Updates all labels in current repository")
-                .conflicts_with("list"),
+        .subcommand(
+            App::new("update")
+                .version(crate_version!())
+                .about("Updates all labels in current repository"),
         )
         .get_matches()
 }
