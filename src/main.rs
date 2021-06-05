cf457e73553a3c92b7c40b6ea3fa42f195e60c4d
ink8bit <ink8bit@users.noreply.github.com>
2021-06-05 21:41:44 +0300

    Improve logic, use async await

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,30 @@ mod cli;
 mod label;
 
 use label::config::Config;
-use label::github;
+use label::github::GitHub;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = cli::args();
     let list = args.is_present("list");
     let update = args.is_present("update");
 
-    let config = match Config::new() {
-        Ok(v) => v,
-        Err(e) => return eprint!("Config file not found: {}", e),
-    };
+    let config = Config::new()?;
 
     let repo = config.repo;
     let owner = config.owner;
 
+    let gh = GitHub::new(&owner, &repo);
+
     if list {
         println!("Labels in repo {}", repo);
-        if let Err(e) = github::print_labels(&owner, &repo) {
-            panic!("{}", e);
-        }
+        gh.print_labels().await?;
     }
 
     if update {
         println!("Updating labels in repo {}", repo);
-        if let Err(e) = github::update_labels(&owner, &repo, &config.labels) {
-            panic!("{}", e);
-        }
+        gh.update_labels(&config.labels).await?;
     }
+
+    Ok(())
 }
