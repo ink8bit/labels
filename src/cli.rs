52d4e460cfcf7547e12d2699ebf54193b692c9b9
ink8bit <ink8bit@users.noreply.github.com>
2022-02-18 22:51:39 +0300

    Update clap to v3.1.0

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,22 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, App};
+use clap::{crate_authors, crate_description, crate_name, crate_version, Command};
 
-pub(crate) fn app() -> App<'static> {
-    App::new(crate_name!())
+pub(crate) fn app() -> Command<'static> {
+    Command::new(crate_name!())
         .version(crate_version!())
         .about(crate_description!())
         .author(crate_authors!())
         .subcommand(
-            App::new("list")
+            Command::new("list")
                 .version(crate_version!())
                 .about("Prints labels in current repository (first 100 items)"),
         )
         .subcommand(
-            App::new("update")
+            Command::new("update")
                 .version(crate_version!())
                 .about("Updates all labels in current repository"),
         )
         .subcommand(
-            App::new("remove")
+            Command::new("remove")
                 .version(crate_version!())
                 .about("Removes all labels in current repository"),
         )
