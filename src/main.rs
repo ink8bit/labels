e7d08d39a678d675a976995daf61428916ae26c6
ink8bit <ink8bit@users.noreply.github.com>
2022-02-19 12:17:24 +0300

    Improve logic of main function

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ async fn main() {
         Err(_) => return eprintln!("Error: no config file found"),
     };
 
-    let repo = config.repo;
-    let owner = config.owner;
+    let repo = &config.repo;
+    let owner = &config.owner;
 
     let token = match env::var(LABELS_TOKEN) {
         Ok(v) => v,
@@ -34,64 +34,13 @@ async fn main() {
 
     let gh = match GitHub::new(API_URL.to_string(), token) {
         Ok(v) => v,
-        Err(e) => {
-            return eprintln!("{}", e);
-        }
+        Err(e) => return eprintln!("{}", e),
     };
 
     match args.subcommand() {
-        Some((LIST_CMD, _)) => {
-            let msg = format!("Getting labels from repo '{}'...", repo);
-            let sp = create_spinner(&msg);
-
-            match gh.print_labels(&owner, &repo).await {
-                Ok(labels) => {
-                    let msg = format!("Labels in repo '{}':", repo);
-                    sp.text(msg);
-                    sp.done();
-                    println!("{}", labels);
-                }
-                Err(e) => {
-                    let err_msg = format!("Error: {}", e);
-                    sp.text(err_msg);
-                    sp.error();
-                }
-            };
-        }
-        Some((UPDATE_CMD, _)) => {
-            let msg = format!("Updating labels in repo '{}'", repo);
-            let sp = create_spinner(&msg);
-
-            match gh.update_labels(&config.labels, &owner, &repo).await {
-                Ok(_) => {
-                    let msg = format!("Successfully updated labels in repo '{}'", repo);
-                    sp.text(msg);
-                    sp.done();
-                }
-                Err(e) => {
-                    let err_msg = format!("Error: {}", e);
-                    sp.text(err_msg);
-                    sp.error();
-                }
-            }
-        }
-        Some((REMOVE_CMD, _)) => {
-            let msg = format!("Removing all labels from repo '{}'", repo);
-            let sp = create_spinner(&msg);
-
-            match gh.remove_labels(&owner, &repo).await {
-                Ok(_) => {
-                    let msg = format!("Successfully removed all labels in repo '{}'", repo);
-                    sp.text(msg);
-                    sp.done();
-                }
-                Err(e) => {
-                    let err_msg = format!("Error: {}", e);
-                    sp.text(err_msg);
-                    sp.error();
-                }
-            }
-        }
+        Some((LIST_CMD, _)) => return print_labels(gh, owner, repo).await,
+        Some((UPDATE_CMD, _)) => return update_labels(gh, config).await,
+        Some((REMOVE_CMD, _)) => return remove_labels(gh, owner, repo).await,
         _ => {
             if let Err(e) = cli::app().print_help() {
                 return eprintln!("{}", e);
@@ -103,4 +52,62 @@ async fn main() {
 fn create_spinner(msg: &str) -> SpinnerHandle {
     let formatted = format!(" {}", msg);
     SpinnerBuilder::new().spinner(&DOTS).text(formatted).start()
+}
+
+async fn print_labels(gh: labels::github::GitHub, owner: &str, repo: &str) {
+    let msg = format!("Getting labels from repo '{}'...", repo);
+    let sp = create_spinner(&msg);
+
+    match gh.print_labels(&owner, &repo).await {
+        Ok(labels) => {
+            let msg = format!("Labels in repo '{}':", repo);
+            sp.text(msg);
+            sp.done();
+            println!("{}", labels);
+        }
+        Err(e) => {
+            let err_msg = format!("Error: {}", e);
+            sp.text(err_msg);
+            sp.error();
+        }
+    }
+}
+
+async fn update_labels(gh: labels::github::GitHub, config: Config) {
+    let msg = format!("Updating labels in repo '{}'", config.repo);
+    let sp = create_spinner(&msg);
+
+    match gh
+        .update_labels(&config.labels, &config.owner, &config.repo)
+        .await
+    {
+        Ok(_) => {
+            let msg = format!("Successfully updated labels in repo '{}'", config.repo);
+            sp.text(msg);
+            sp.done();
+        }
+        Err(e) => {
+            let err_msg = format!("Error: {}", e);
+            sp.text(err_msg);
+            sp.error();
+        }
+    }
+}
+
+async fn remove_labels(gh: labels::github::GitHub, owner: &str, repo: &str) {
+    let msg = format!("Removing all labels from repo '{}'", repo);
+    let sp = create_spinner(&msg);
+
+    match gh.remove_labels(&owner, &repo).await {
+        Ok(_) => {
+            let msg = format!("Successfully removed all labels in repo '{}'", repo);
+            sp.text(msg);
+            sp.done();
+        }
+        Err(e) => {
+            let err_msg = format!("Error: {}", e);
+            sp.text(err_msg);
+            sp.error();
+        }
+    }
 }
